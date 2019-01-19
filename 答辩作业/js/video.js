4b17658d5056b6eec5106c29be5e822b32f48499
LowApe <didiaoyuan@gmail.com>
2019-01-19 09:06:02 +0800

    2019-01-19 09:05 解决一些错误

--- 答辩作业/js/video.js
+++ 答辩作业/js/video.js
@@ -1,9 +1,17 @@
-$(function(){ //当页面加载时
-    task = setInterval("loadImg()" , 2000); //启动定时器
-    $("#img").mouseover(function(){ //鼠标经过
+let index = 1;
+let task;
+function loadImg() {
+    if (index === 4) index = 1;
+    $("#img").css("background-image", "url(img/v" + index + ".jpg)");
+    index++;
+}
+$(function () { //当页面加载时
+
+    task = setInterval("loadImg()", 1500); //启动定时器
+    $("#img").mouseover(function () { //鼠标经过
         window.clearInterval(task);
     });
-    $("#img").mouseout(function(){ //鼠标移出
-        task = setInterval("loadImg()", 2000);
+    $("#img").mouseout(function () { //鼠标移出
+        task = setInterval("loadImg()", 1500);
     })
 })
